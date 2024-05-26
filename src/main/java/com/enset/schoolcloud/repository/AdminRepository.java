8daddcb30e044dc1714bbdf23ac768c86a727927
nickKengne <nickdk294@gmail.com>
2024-05-26 15:49:29 +0100

    add config for authentication json web token

--- src/main/java/com/enset/schoolcloud/repository/AdminRepository.java
+++ src/main/java/com/enset/schoolcloud/repository/AdminRepository.java
@@ -3,5 +3,8 @@ package com.enset.schoolcloud.repository;
 import com.enset.schoolcloud.entity.Admin;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface AdminRepository extends JpaRepository<Admin, Integer> {
+    Optional<Admin> findByEmail(String email);
 }
